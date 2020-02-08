5b7fd1a17b1a15bbe9d4edec7efde26d8c374a29
daGu29 <1031472162@qq.com>
2020-02-09 01:01:01 +0800

    公共类

---
+++ src/main/java/com/mx/learn/common/Const.java
@@ -0,0 +1,12 @@
+package com.mx.learn.common;
+
+/**
+ * Const
+ *
+ * @author dagu29
+ * @date 2020/2/9 0009
+ */
+public class Const {
+
+    public static final String CURRENT_USER = "currentUser";
+}
---
+++ src/main/java/com/mx/learn/common/ResponseCode.java
@@ -0,0 +1,30 @@
+package com.mx.learn.common;
+
+/**
+ * ResponseCode
+ *
+ * @author dagu29
+ * @date 2020/2/8 0008
+ */
+public enum ResponseCode {
+    SUCCESS(0,"SUCCESS"),
+    ERROR(1,"ERROR"),
+    NEED_LOGIN(10,"NEED_LOGIN"),
+    ILLEGAL_ARGUMENT(2,"ILLEGAL_ARGUMENT");
+
+    private final int code;
+    private final String desc;
+
+    ResponseCode(int code,String desc){
+        this.code=code;
+        this.desc=desc;
+    }
+
+    public int getCode(){
+        return code;
+    }
+
+    public String getDesc(){
+        return desc;
+    }
+}
