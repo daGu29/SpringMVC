36e20ba78698176550498e3f5064e67713a6bfae
daGu29 <1031472162@qq.com>
2020-02-11 00:48:57 +0800

    用户模块代码

--- src/main/java/com/mx/learn/service/IUserService.java
+++ src/main/java/com/mx/learn/service/IUserService.java
@@ -1,5 +1,8 @@
 package com.mx.learn.service;
 
+import com.mx.learn.common.ServerResponse;
+import com.mx.learn.pojo.User;
+
 /**
  * IUserService
  *
@@ -7,4 +10,21 @@ package com.mx.learn.service;
  * @date 2020/2/8 0008
  */
 public interface IUserService {
+    ServerResponse<User> login(String username, String password);
+
+    ServerResponse<String> register(User user);
+
+    ServerResponse<String> checkValid(String str, String type);
+
+    ServerResponse selectQuestion(String username);
+
+    ServerResponse<String> checkAnswer(String username,String question,String answer);
+
+    ServerResponse<String> forgetResetPassword(String username, String passwordNew, String forgetToken);
+
+    ServerResponse<String> resetPassword(String passwordOld,String passwordNew,User user);
+
+    ServerResponse<User> updateInformation(User user);
+
+    ServerResponse<User> getInformation(Integer userId);
 }
